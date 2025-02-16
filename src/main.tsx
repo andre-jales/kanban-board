33541a194047532acbeb3faa73b2680fcd8f66b2
andre-jales <andrejales.com@gmail.com>
2025-02-16 16:10:38 -0300

    Wrap App component with ThemeProvider to apply dark theme using MUI

--- src/main.tsx
+++ src/main.tsx
@@ -1,10 +1,21 @@
+import { ThemeProvider } from "@emotion/react";
 import { StrictMode } from "react";
 import { createRoot } from "react-dom/client";
+import { createTheme } from "@mui/material/styles";
+
 import "./index.css";
 import App from "./App.tsx";
 
+const theme = createTheme({
+  palette: {
+    mode: "dark",
+  },
+});
+
 createRoot(document.getElementById("root")!).render(
   <StrictMode>
-    <App />
+    <ThemeProvider theme={theme}>
+      <App />
+    </ThemeProvider>
   </StrictMode>
 );
